a8b80a3b4d623c0c48e97c9d49ea3f98d62200f9
cryptic-deer <artist.tibi@gmail.com>
2022-05-17 18:52:19 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/** @format */
+
+const binaryInput = document.querySelector(".binary");
+const decimalOutput = document.querySelector(".decimal");
+
+binaryInput.addEventListener("input", e => {
+	e.preventDefault();
+
+	setTimeout(() => {
+		let binaryString = e.target.value;
+
+		// Testing for valid binary number
+		if (/[^01]/.test(binaryString)) {
+			decimalOutput.classList.add("input", "error");
+
+			return (decimalOutput.textContent = "Invalid digits");
+		} else if (binaryString === "") {
+			// If input is empty
+			decimalOutput.classList.remove("error");
+			decimalOutput.classList.add("input");
+
+			return (decimalOutput.textContent = "...");
+		}
+
+		decimalOutput.classList.add("input");
+		decimalOutput.classList.remove("error");
+
+		decimalOutput.textContent = binToDec(binaryString);
+	}, 1000);
+});
+// Converting binary number to decimal
+const binToDec = binaryString => {
+	return parseInt((binaryString + "").replace(/[^01]/gi, ""), 2);
+};
